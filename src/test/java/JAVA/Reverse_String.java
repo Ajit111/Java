e3f5fdf415d57cd9ce27b0fcfefdefbe42a6fb54
Ajit kumar <59956826+Ajit111@users.noreply.github.com>
2025-11-01 16:34:02 +0530

    Refactor string reversal logic in Reverse_String.java

--- src/test/java/JAVA/Reverse_String.java
+++ src/test/java/JAVA/Reverse_String.java
@@ -3,17 +3,16 @@ package JAVA;
 public class Reverse_String {
 
 	public static void main(String[] args) {
-		String originalStr = "Ajit";
-		
-		// Convert to uppercase
-		originalStr = originalStr.toLowerCase();
-		
-		String ReversedStr = "";
-		for (int i = 0; i < originalStr.length(); i++) {
-			ReversedStr = originalStr.charAt(i) + ReversedStr;
-		}
-		System.out.println(ReversedStr);
+	String originalStr="Ajit";
+	String reversedStr="";
+	for (int i=0;i<originalStr.length();i++) {
+		reversedStr=originalStr.charAt(i)+reversedStr;
 		
 	}
-}
+		
+	System.out.println(reversedStr);
+		
 
+	}
+
+}
