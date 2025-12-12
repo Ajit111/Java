d410b4961c1a68afeb6bc854a942216fbb1539d4
Ajit kumar <59956826+Ajit111@users.noreply.github.com>
2025-12-12 18:03:14 +0530

    Refactor CharacterCount for case-insensitive counting

--- src/test/java/JAVA/CharacterCount.java
+++ src/test/java/JAVA/CharacterCount.java
@@ -1,29 +1,16 @@
-package JAVA;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-public class CharacterCount {
-	public static void main(String[] args) {
-        String test = "Test";
-        test = test.toUpperCase();  // case-insensitive count ke liye
-
-        Map<Character, Integer> map = new LinkedHashMap<>();
-
-        // count each character using normal for loop
-        for (int i = 0; i < test.length(); i++) {
-            char c = test.charAt(i);
-            if (Character.isLetter(c)) {  // sirf letters count karna hai
-                map.put(c, map.getOrDefault(c, 0) + 1);
-            }
-        }
-
-        // display result using normal for loop
-        for (int i = 0; i < map.size(); i++) {
-            Character key = (Character) map.keySet().toArray()[i];
-            Integer value = map.get(key);
-            System.out.print(key + "=" + value + ", ");
-        }
-    }
-}
-//output =T=2, E=1, S=1, 
+public class CharacterCount{
+	public static void main(String []args) {
+		String str="Test";
+		
+		str=str.toLowerCase();
+		Map<Character,Integer> map= new LinkedHashMap<>();
+		for (int i=0;i<str.length();i++) {
+			char c =str.charAt(i);
+			int count =map.getOrDefault(c,0);
+			map.put(c, count+1);
+		}
+		for (char key : map.keySet()) {
+			System.out.println(key+"="+map.get(key));		
+		}
+	}
+}	
